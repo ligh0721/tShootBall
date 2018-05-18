27a83f6d7bb2e5b43a7d45e120a801e61c30feb9
jing.liu <jing.liu@yff.com>
2018-05-18 16:55:18 +0800

    add background

--- src/Main.ts
+++ src/Main.ts
@@ -99,7 +99,7 @@ class Main extends eui.UILayer {
      * Create scene interface
      */
     protected createGameScene(): void {
-        console.log("stage: "+this.stage.stageWidth+"x"+this.stage.stageHeight);
+        tutils.Layer.createAt(TestLayer, this);
     }
     /**
      * 根据name关键字创建一个Bitmap对象。name属性请参考resources/resource.json配置文件的内容。
--- src/layer/TestLayer.ts
+++ src/layer/TestLayer.ts
@@ -1,5 +1,17 @@
 class TestLayer extends tutils.Layer {
+	world: p2.World;
+
 	protected onInit(): void {
-		let world = new p2.World();
+		let bg = tutils.createBitmapByName("grid100_png");
+		this.addChild(bg);
+		this.createWorld();
 	}
+
+	private createWorld(): p2.World {
+        let world: p2.World = new p2.World();
+        world.sleepMode = p2.World.BODY_SLEEPING;
+        world.gravity = [0, 10];
+        this.world = world;
+		return world;
+    }
 }
